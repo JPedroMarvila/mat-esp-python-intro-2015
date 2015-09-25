2b6c9014692a36aea199634f3e06bec1b5df70ae
leovsf <leo_v_sf@hotmail.com>
2015-09-25 10:47:38 -0300

    plotamos os graficos original e ordenado

--- Joao-Paulo-Leonardo/bubble-sort.py
+++ Joao-Paulo-Leonardo/bubble-sort.py
@@ -1,9 +1,20 @@
+import matplotlib.pyplot as plt
+
 #cartas embaralhadas
 cartas = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
 #imprimir a lista cartas
 print("lista original:", cartas)
 #numero de cartas
 n = 20
+
+#plotamos um grafico com as infos originais
+plt.figure()
+plt.plot(range(n), cartas, 'ok')
+plt.title("grafico lista original")
+plt.xlabel("numero de cartas")
+plt.ylabel("valores das cartas")
+plt.savefig("bubble-inicio.png")
+
 #percorre com o i
 for i in range(0, n - 1, 1):
 #percorre com o j
@@ -23,5 +34,15 @@ print ("cinco maiores valores:", cartas[n: n - 5: -1])
 #imprime os cinco menores valores
 #print (cartas[20: 15: -1] 
 print ("cinco menores valores:", cartas[0: 5: 1])
+
+#plotamos um grafico com as infos ordenadas
+plt.figure()
+plt.plot(range(n), cartas, 'ok')
+plt.title("grafico lista ordenada")
+plt.xlabel("numero de cartas")
+plt.ylabel("valores das cartas")
+plt.savefig("bubble-fim.png")
+
+print ("Matematica {} - Turma {}".format("Especial", 1))
 
 
